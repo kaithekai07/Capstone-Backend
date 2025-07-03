40bfd534eb4a8d5b8e957a69871f952eca018f7e
SafeSightAI <zhunjietan@gmail.com>
2025-07-03 10:44:03 +0800

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ from flask import Flask, request, jsonify, render_template, url_for
 from flask_cors import CORS
 
 app = Flask(__name__, static_folder="static", static_url_path="/static")
-CORS(app, resources={r"/*": {"origins": "https://safesightai.vercel.app"}})
+CORS(app, resources={r"/*": {"origins": "*"}})
 
 
 
