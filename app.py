16eaa3fd0cc406e0436f5edac375185137feccf1
SafeSightAI <zhunjietan@gmail.com>
2025-07-17 13:25:12 +0800

    Update app.py

--- app.py
+++ app.py
@@ -259,7 +259,7 @@ def analyze():
         except:
             total_cost = 0
 
-        supabase.table("merged_car_reports").insert({
+supabase.table("merged_car_reports").upsert({
             "car_id": car_id,
             "description": car_desc,
             "date": car_date,
