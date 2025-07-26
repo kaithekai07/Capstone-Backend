c1e0df5370ce5ab4400db8a4b5da47de31013e78
SafeSightAI <zhunjietan@gmail.com>
2025-07-26 22:56:35 +0800

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ from pathlib import Path
 import traceback
 import re
 import json
-pip install -U sentence-transformers
+from sentence_transformers import SentenceTransformer, util
 
 app = Flask(__name__)
 CORS(app, origins=["https://safesightai.vercel.app"], supports_credentials=True)
